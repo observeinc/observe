184b41e04fbd3fd88f22b53d78e610cf9cc92ced
Jon Watte <jwatte@observeinc.com>
2023-05-10 09:56:41 -0700

    Try to make code signing actually work as part of the release.

--- s/apple-sign.sh
+++ s/apple-sign.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "usage: s/apple-sign.sh artifact-path.zip"
+    exit 1
+fi
 set -x
 xcrun notarytool submit "$1" --keychain-profile "notarytool-jwatte-gmail-observeinc-team" --wait
